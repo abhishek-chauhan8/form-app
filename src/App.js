faa49f246c4604c52e26b22a61332a54da557616
Abhishek Chauhan <rajputankur882@gmail.com>
2024-08-24 13:17:47 +0530

    form-app-first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  // here we're handling the state individually
+
+  // const[firstName, setFirstName] = useState("");
+  // const[lastName, setLastName] = useState("");
+
+  // function changeFirstNameHandler (event){
+  //   setFirstName(event.target.value)
+  // }
+
+  // function changeLastNameHandler(event){
+  //   setLastName(event.target.value);
+  // }
+
+  // here states are handled in an array and object of states is passed into them
+
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  function changeHandler(event) {
+    setFormData((prevStateData) => {
+      console.log(formData);
+      return {
+        ...prevStateData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input
+          type="text"
+          className="border-4 border-dashed border-black"
+          placeholder="First Name"
+          name="firstName"
+          onChange={changeHandler}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          className="border-4 border-dashed border-black"
+          placeholder="Last Name"
+          name="lastName"
+          onChange={changeHandler}
+        />
+        <br />
+        <br />
+        <input
+          type="text"
+          className="border-4 border-dashed border-black"
+          placeholder="Enter your email"
+          name="email"
+          onChange={changeHandler}
+        />
+      </form>
     </div>
   );
 }
